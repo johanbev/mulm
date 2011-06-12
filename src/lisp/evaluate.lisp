69bdb32f6bcc201f513365710a338972f70e088b
Andre Lynum <and-ly@online.no>
2011-06-12 15:37:04 +0200

    Quick function to evaluate HMM tagger.

--- src/lisp/evaluate.lisp
+++ src/lisp/evaluate.lisp
@@ -22,3 +22,16 @@
                         and do (setf forms nil)
                         and do (setf tags nil)
                         finally (return (/ correct total)))))
+
+;; Quick function to run standard evaluation
+(defun evaluate-all ()
+  (time (evaluate-hmm (train-hmm (read-corpus *tagger-train-file*))
+                      *tagger-eval-file*)))
+
+;; Results on LW6 pro, Core 2 MacBook Pro (André)
+;; User time    =       10.351
+;; System time  =        0.071
+;; Elapsed time =       10.388
+;; Allocation   = 430045312 bytes
+;; 1616 Page faults
+;; 11383/11890
--- src/lisp/mulm.lisp
+++ src/lisp/mulm.lisp
@@ -3,3 +3,19 @@
 (defpackage :mulm)
 
 (in-package :mulm)
+
+;; store the root path of the lisp code directory
+(defparameter *package-path*
+  (directory-namestring
+   (asdf:component-pathname (asdf:find-system :mulm))))
+
+;; default placement of evaluation corpora is beside the src directory
+(defparameter *eval-path*
+  (merge-pathnames "../../eval/" *package-path*))
+
+;; POS tagging evaluation corpus
+(defparameter *tagger-train-file*
+  (merge-pathnames "wsj.tt" *eval-path*))
+
+(defparameter *tagger-eval-file*
+  (merge-pathnames "test.tt" *eval-path*))
