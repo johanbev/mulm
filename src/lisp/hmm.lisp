eba75f922a3fe003ba1fbfcfb966fb640835f3c9
Johan Benum Evensberget <johan.benum@gmail.com>
2011-06-13 15:12:36 +0200

    Experimental beam-viterbi

--- src/lisp/hmm.lisp
+++ src/lisp/hmm.lisp
@@ -181,3 +181,78 @@
         for state = (aref pointer last i) then (aref pointer state i)
         do (push (elt tags state) result)
         finally (return result))))
+
+(defun beam-viterbi (hmm input &key (beam-width 1.707))
+  (declare (optimize (speed 3) (debug  0) (space 0)))
+  (setf beam-width (float beam-width))
+  (let* ((n (hmm-n hmm))
+         (l (length input))
+         (viterbi (make-array (list n l) :initial-element most-negative-single-float))
+         (pointer (make-array (list n l) :initial-element nil)))
+    ;;; Array initial element is not specified in standard, so we carefully
+    ;;; specify what we want here. ACL and SBCL usually fills with nil and 0 respectively.
+    (declare (type fixnum n l)
+	     (type single-float beam-width))
+    (loop
+        with form of-type fixnum = (first input)
+        for state of-type fixnum from 0 to (- n 1)
+        do
+          (setf (aref viterbi state 0)
+            (+ (transition-probability hmm 0 state)
+               (emission-probability hmm state form)))
+          (setf (aref pointer state 0) 0))
+    (loop
+	for form of-type fixnum in (rest input)
+	for time of-type fixnum from 1 to (- l 1)
+	with indices = (make-array n :initial-contents (loop for i from 0 to (1- n) collect i) :fill-pointer t)
+	for best-hypothesis of-type single-float = most-negative-single-float
+	for trigger of-type single-float = most-negative-single-float
+        do
+	  (loop	     
+	      for current of-type fixnum from 0 to (1- n)
+	      do
+		(loop
+		    with old of-type single-float = (aref viterbi current time)
+		    with prev-time fixnum = (1- time)
+		    for index fixnum from 0 to (1- (fill-pointer indices))
+		    for previous = (aref indices index)
+		    for prev-prob of-type single-float = (aref viterbi previous prev-time)
+		    when (> prev-prob old)
+		    do
+		      (let ((new (+ prev-prob
+				    (the single-float (transition-probability hmm previous current))
+				    (emission-probability hmm current form))))
+			(declare (type single-float new))
+			(when (> new trigger)
+			  (setf trigger new)
+			  (setf best-hypothesis (- new beam-width)))
+			(when (> new old)
+			  (setf old new)
+			  (setf (aref viterbi current time) new)
+			  (setf (aref pointer current time) previous)))))
+	  (loop
+	      initially (setf (fill-pointer indices) 0)
+	      for current of-type fixnum from 0 to (- n 1)
+	      for prob of-type single-float = (the single-float (aref viterbi current time))
+	      when (> prob best-hypothesis)
+	      do (vector-push current indices)))
+    (loop
+	with final = (tag-to-code hmm "</s>")
+	with time of-type fixnum = (- l 1)
+        for previous of-type fixnum from 0 to (- n 1)
+        for old of-type single-float = (aref viterbi final time)
+        for new of-type single-float = (+ (the single-float (aref viterbi previous time))
+                     (transition-probability hmm previous final))
+        when (or (null old) (> new old)) do
+          (setf (aref viterbi final time) new)
+          (setf (aref pointer final time) previous))
+    (loop
+	with final = (tag-to-code hmm "</s>")
+	with time = (- l 1)
+        with last = (aref pointer final time)
+        with tags = (hmm-tags hmm)
+        with result = (list (elt tags last))
+        for i of-type fixnum from time downto 1
+        for state = (aref pointer last i) then (aref pointer state i)
+        do (push (elt tags state) result)
+        finally (return result))))
