cb45a7bb30c86632cb54cecdc56d492df7f80ca6
Andre Lynum <and-ly@online.no>
2011-07-04 11:18:40 +0200

    Slow path decoder for development and debugging. Decoder structure for encapsulating decoder, model and runtime data/caching.

--- src/lisp/hmm-decoder.lisp
+++ src/lisp/hmm-decoder.lisp
@@ -2,7 +2,7 @@
 
 (defvar *bigrams* nil)
 
-(defun viterbi-trigram (hmm input &key (bigrams *bigrams*))
+(defun viterbi-trigram (hmm input &key (bigrams *bigrams*) &allow-other-keys)
   (declare (optimize (speed 3) (debug  1) (space 0)))
   (let* ((n (hmm-n hmm))
          (nn (* n n))
@@ -117,8 +117,8 @@
                     (mapcar #'second result)))))
 
 ;;; hvor conser denne?
-(defun viterbi-bigram (hmm input &key (beam-width 13.80))
-  (declare (optimize (speed 3) (debug  0) (space 0)))
+(defun viterbi-bigram (hmm input &key (beam-width 13.80) &allow-other-keys)
+  (declare (optimize (speed 3) (debug  1) (space 0)))
   (let* ((n (hmm-n hmm))
          (l (length input))
          (viterbi (make-array (list n l) :initial-element most-negative-single-float :element-type 'single-float))
@@ -177,6 +177,106 @@
                           (setf old new)
                           (setf (aref viterbi current time) new)
                           (setf (aref pointer current time) previous)))))
+          (loop
+              initially (setf (fill-pointer indices) 0)
+              for current of-type fixnum from 0 to (- n 1)
+              for prob of-type single-float = (the single-float (aref viterbi current time))
+              when (> prob best-hypothesis)
+              do (vector-push current indices)))
+
+    (loop
+        with final = (tag-to-code hmm "</s>")
+        with time of-type fixnum = (- l 1)
+        for previous of-type fixnum from 0 to (- n 1)
+        for old of-type single-float = (aref viterbi final time)
+        for new of-type single-float = (+ (the single-float (aref viterbi previous time))
+                                          (bi-cached-transition hmm previous final))
+        when (> new old) do
+          (setf (aref viterbi final time) new)
+          (setf (aref pointer final time) previous))
+    (if (null (aref pointer (tag-to-code hmm "</s>") (- l 1)))
+        nil
+      (loop
+          with final = (tag-to-code hmm "</s>")
+          with time = (- l 1)
+          with last  = (aref pointer final time)
+          with tags = (hmm-tags hmm)
+          with result = (list (elt tags last))
+          for i of-type fixnum from time downto 1
+          for state = (aref pointer last i) then (aref pointer state i)
+          never (null state)
+          do (push (elt tags state) result)
+          finally (return result)))))
+
+(defstruct viterbi-decoder
+  function
+  model
+  (caches (make-hash-table)))
+
+(defun make-decoder (training-corpus)
+  (init-decoder
+   (make-viterbi-decoder :function #'viterbi-bigram-slow
+                         :model (train training-corpus))))
+
+(defun init-decoder (decoder)
+  (setf (gethash :unknown-word (viterbi-decoder-caches decoder))
+        (make-hash-table))
+  decoder)
+
+(defun viterbi-bigram-slow (hmm input &key (beam-width 13.80) (decoder nil))
+  (declare (ignore hmm))
+  ; (declare (optimize (speed 3) (debug  1) (space 0)))
+  (let* ((hmm (viterbi-decoder-model decoder))
+         (n (hmm-n hmm))
+         (l (length input))
+         (viterbi (make-array (list n l)
+                              :initial-element most-negative-single-float
+                              :element-type 'single-float))
+         (pointer (make-array (list n l) :initial-element nil)))
+    ;;; Array initial element is not specified in standard, so we carefully
+    ;;; specify what we want here. ACL and SBCL usually fills with nil and 0 respectively.
+    (declare (type fixnum n l))
+    (declare (type (simple-array single-float (* *)) viterbi)
+             (type (simple-array t (* *)) pointer))
+    (init-decoder decoder)
+    (loop
+        with form of-type fixnum = (first input)
+        for state of-type fixnum from 0 to (- n 1)
+        do (setf (aref viterbi state 0)
+                 (+ (bi-cached-transition hmm (tag-to-code hmm "<s>") state)
+                    (emission-probability-slow decoder hmm state form)))
+        do (setf (aref pointer state 0) 0))
+    (loop
+        for form of-type fixnum in (rest input)
+        for time of-type fixnum from 1 to (- l 1)
+        with indices = (hmm-beam-array hmm)
+        initially (setf (fill-pointer indices) 0)
+                  (loop 
+                      for x below n
+                      do (vector-push x indices))
+        for best-hypothesis of-type single-float = most-negative-single-float
+        for trigger of-type single-float = most-negative-single-float
+        do (loop
+            for current of-type fixnum from 0 to (- n 1)
+            do (loop
+                with old of-type single-float = (aref viterbi current time)
+                with emission of-type single-float = (emission-probability-slow decoder hmm current form)
+                for index fixnum from 0 to (1- (fill-pointer indices))
+                for previous = (aref indices index)
+                for prev-prob of-type single-float = (aref viterbi previous   (- time 1))
+                when (> prev-prob old) do
+                (let ((new
+                       (+ prev-prob
+                          (bi-cached-transition hmm previous current)
+                          emission)))
+                  (declare (type single-float new))
+                  (when (> new trigger)
+                    (setf trigger new)
+                    (setf best-hypothesis (- new beam-width)))
+                  (when (> new old)
+                    (setf old new)
+                    (setf (aref viterbi current time) new)
+                    (setf (aref pointer current time) previous)))))
           (loop
               initially (setf (fill-pointer indices) 0)
               for current of-type fixnum from 0 to (- n 1)
