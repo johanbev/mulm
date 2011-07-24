e3ace86c6c1656eb9dfe45361a61fc24432cd9fc
Andre Lynum <and-ly@online.no>
2011-07-24 15:23:54 +0200

    Removed dynamic-extent declaration again.

--- src/lisp/word-model.lisp
+++ src/lisp/word-model.lisp
@@ -187,8 +187,6 @@
          (accu-weight 0.0)
          (prob (make-array (hmm-n hmm) :initial-element nil)))
     (declare (type (simple-array t (*)) prob))
-    ;; sbcl compiles with error with this declaration
-    #-sbcl (declare (dynamic-extent nodes))
     (loop
      for seq in (nreverse (loop for seq on nodes collect seq)) ;; Loop from small to big
      for i fixnum from 0
