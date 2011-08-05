a8a7883b077394050c40560a4cc0e9ad636e7093
johan benum evensberget <johan.benum@gmail.com>
2011-08-05 19:16:18 +0200

    Fixed bug where start and stop states where doubly accounted

--- src/lisp/count-tree.lisp
+++ src/lisp/count-tree.lisp
@@ -32,12 +32,62 @@
     (loop
         for word fixnum in sentence
         for seq = (fast-queue-to-list (fast-enqueue buffer word))
+        when (>= (length seq) n)
         do (add-sequence seq lm-root)
         finally (loop 
                     for x on (rest seq)
                     do (add-sequence x lm-root)))))
 
 (defun build-model (sentences n lm-root)
-  (loop 
+  (loop
       for sent in sentences
       do (sentence-to-n-grams sent n lm-root)))
+
+
+(defun check-lm (corpus)
+  (let* ((hmm (setup-hmm (make-hmm) (corpus-tag-set-size corpus)))
+        (n (hmm-n hmm))
+         (lm-root (make-lm-tree-node)))
+    (populate-counts corpus hmm)
+    (loop initially
+          (build-model (mapcar (lambda (x) (append (list (token-to-code "<s>" (hmm-tag-lexicon hmm) :rop t))
+                                                   (mapcar (lambda (x) (token-to-code x (hmm-tag-lexicon hmm) :rop t)) x)
+                                                   (list (token-to-code  "</s>" (hmm-tag-lexicon hmm) :rop t))))
+                               (ll-to-tag-list corpus)) 3 lm-root)
+        for t1 from 0 below n
+        for t1-node = (getlash t1 (lm-tree-node-children lm-root))
+        when (and (not t1-node) (/= (aref (hmm-unigram-counts hmm) t1) 0))
+        do (format t "Inconsistent unigram count ~a - ~a - ~a~%" t1
+                   0 (aref (hmm-unigram-counts hmm) t1))
+        when t1-node do
+          (if (/= (lm-tree-node-total t1-node) (aref (hmm-unigram-counts hmm) t1))
+              (format t "Inconsistent unigram count ~a - ~a - ~a~%" t1
+                      (lm-tree-node-total t1-node) (aref (hmm-unigram-counts hmm) t1)))
+        and do
+            (loop
+                for t2 from 0 below n
+                for t2-node = (getlash t2 (lm-tree-node-children t1-node))
+                when (and (not t2-node) (/= (aref (hmm-bigram-counts hmm) t1 t2) 0))
+                do (format t "Inconsistent bigram count ~a ~a - ~a - ~a~%" t1 t2
+                           0 (aref (hmm-bigram-counts hmm) t1 t2))
+                when t2-node do
+                  (if (/= (lm-tree-node-total t2-node) (aref (hmm-bigram-counts hmm) t1 t2))
+                      (format t "Inconsistent bigram count ~a ~a - ~a - ~a~%" t1 t2
+                              (lm-tree-node-total t2-node) (aref (hmm-bigram-counts hmm) t1 t2)))
+                and do
+                    (loop
+                        with total = (float (lm-tree-node-total t2-node))
+                        for t3 from 0 below n
+                        for t3-node = (getlash t3 (lm-tree-node-children t2-node))
+                        when (and (not t3-node) (/= (aref (hmm-trigram-counts hmm) t1 t2 t3) 0))
+                        do (format t "Inconsistent bigram count ~a ~a ~a - ~a - ~a~%" t1 t2 t3
+                                   0 (aref (hmm-trigram-counts hmm) t1 t2 t3))
+                        when t3-node do
+                          (if (/= (lm-tree-node-total t3-node) (aref (hmm-trigram-counts hmm) t1 t2 t3))
+                              (format t "Inconsistent trigram count ~a ~a ~a - ~a - ~a~%" t1 t2 t3
+                                      (lm-tree-node-total t3-node) (aref (hmm-trigram-counts hmm) t1 t2 t3)))
+                        and do
+                            (let ((prob (/ (lm-tree-node-total t3-node)
+                                           total)))
+                              (setf (aref (hmm-trigram-table hmm) t1 t2 t3)
+                                prob)))))))
