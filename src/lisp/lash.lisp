60826fcfb3cd1f0f2ba24775b1c84b42229aeb32
Johan Benum Evensberget <johan.benum@gmail.com>
2011-08-10 21:13:10 +0200

    added keys on various hash/lash utils

--- src/lisp/lash.lisp
+++ src/lisp/lash.lisp
@@ -1,4 +1,4 @@
-(in-package :mulm)
+r(in-package :mulm)
 
 (defstruct lash
   table)
@@ -57,39 +57,40 @@
       (list (length lash-internal))
       (hash-table (hash-table-count lash-internal)))))
 
-(defun lash-table-sum (lash)
+(defun lash-table-sum (lash &key (key #'identity))
   (let ((lash-internal (lash-table lash)))
     (if lash-internal
         (if (and (listp lash-internal) (null (cdr lash-internal)))
-            (cdar lash-internal)
+            (funcall key (cdar lash-internal))
           (let ((sum 0))
             (maplash (lambda (k v)
                        (declare (ignore k))
-                       (incf sum (the fixnum v)))
+                       (incf sum (funcall key v)))
                      lash)
             sum))
       0)))
 
-(defun lash-table-entropy (lash)
+(defun lash-table-entropy (lash &key (key #'identity))
   (let ((lash-internal (lash-table lash)))
     (etypecase lash-internal
       (list
-       (let* ((sum (float (lash-table-sum lash)))
+       (let* ((sum (float (lash-table-sum lash :key key)))
               (accu 0.0))
          (declare (type single-float accu sum))
          (loop
-          for v in (lash-values lash)
-          for div of-type single-float = (if (= sum 0.0) ;; division by zero guard
-                                           0.0
-                                           (/ v sum))
-          do
-          (incf accu (* -1.0 (the single-float div)
-                        (if (= div 0.0) ;; log of zero guard
-                          0
-                          (log div 2)))))            
+             for raw in (lash-values lash)
+             for v = (funcall key raw)
+             for div of-type single-float = (if (= sum 0.0) ;; division by zero guard
+                                                0.0
+                                              (/ v sum))
+             do
+               (incf accu (* -1.0 (the single-float div)
+                             (if (= div 0.0) ;; log of zero guard
+                                 0
+                               (log div 2)))))            
          accu))
       (hash-table
-       (hash-table-entropy lash-internal)))))
+       (hash-table-entropy lash-internal :key key)))))
 
 (defun lash-keys (lash)
     (let (accu)
