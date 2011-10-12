b5defb4d51b202ff3c3d305e36c1ffd8ad340ae3
andrely <and-ly@online.no>
2011-10-12 12:22:36 +0200

    Moved process-sentence().

--- src/lisp/delivery-top-level.lisp
+++ src/lisp/delivery-top-level.lisp
@@ -1,17 +1,5 @@
 (in-package :cl-user)
 
-(defun process-sentence (sentence decoder)
-  (let* ((tokens (mapcar #'mulm::token-internal-form sentence))
-         (result (mulm::decode decoder tokens))
-         (external-forms (loop for token in sentence
-                               if (mulm::token-external-form token)
-                               collect (mulm::token-external-form token)
-                               else collect (mulm::token-internal-form token))))
-    (loop for tok in external-forms
-          for tag in result
-          do (format t "~a~C~a~%" tok #\Tab tag))
-    (format t "~%")))
-
 ;; simple top level driver for command line delivery
 (defun top-level-tag (model-file in-file)
   (let* ((model (mulm::deserialize-hmm-model-from-file model-file))
@@ -23,7 +11,7 @@
 
     (mulm::read-tt-corpus in-file
                           :sentence-handler #'(lambda (sentence)
-                                                (process-sentence sentence decoder))
+                                                (mulm::process-sentence sentence decoder))
                           :collect nil)
 
     (log5:log-for (log5:info) "Completed tagging corpus")))
--- src/lisp/hmm-decoder.lisp
+++ src/lisp/hmm-decoder.lisp
@@ -338,6 +338,18 @@
 (defun decode (decoder sentence)
   (funcall (decoder-function decoder) decoder sentence))
 
+(defun process-sentence (sentence decoder)
+  (let* ((tokens (mapcar #'mulm::token-internal-form sentence))
+         (result (decode decoder tokens))
+         (external-forms (loop for token in sentence
+                               if (token-external-form token)
+                               collect (token-external-form token)
+                               else collect (token-internal-form token))))
+    (loop for tok in external-forms
+          for tag in result
+          do (format t "~a~C~a~%" tok #\Tab tag))
+    (format t "~%")))
+
 (defun decode-start (decoder hmm input viterbi pointer &optional (constraints nil))
   (let ((n (hmm-tag-cardinality hmm)))
     (loop
