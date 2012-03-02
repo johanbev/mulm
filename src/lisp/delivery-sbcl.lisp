25a822442939e429bf34bd012a359a95aacde676
johan benum evensberget <johan.benum@gmail.com>
2012-03-02 14:05:20 +0100

    remove compile directive in delivery-sbcl

--- src/lisp/delivery-sbcl.lisp
+++ src/lisp/delivery-sbcl.lisp
@@ -2,12 +2,12 @@
 (in-package :cl-user)
 
 ;; load and setup asdf
-(load (compile-file "asdf-setup.lisp"))
+;(load (compile-file "asdf-setup.lisp"))
 
 (asdf:operate 'asdf:load-op :mulm :force t)
 (asdf:operate 'asdf:load-op :getopt :force t)
 
-(load (compile-file "delivery-top-level.lisp"))
+(load "delivery-top-level.lisp")
 
 ;; dummy main function giving the code a short workout
 (save-lisp-and-die #p"mulm-sbcl-test" :executable t :toplevel #'main)
