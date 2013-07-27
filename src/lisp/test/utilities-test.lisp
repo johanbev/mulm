b32b40bf758aebde5bcb844fa2bf8ef68ae81f34
Johan Benum Evensberget <johan.benum@gmail.com>
2013-07-27 15:34:29 +0200

    merge

--- src/lisp/test/utilities-test.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :mulm-test)
-
-(deftestsuite utilities (all) ()
-  (:tests
-   (tally
-    (ensure-same (sort (mulm::tally '(1 2 2 3 3 3 4 4 4 4)) #'< :key #'first)
-                 '((1 . 1) (2 . 2) (3 . 3) (4 . 4))))))
