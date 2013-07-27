f156b970ce8fe8aed763625adcfff73dc4bda070
andrely <and-ly@online.no>
2013-07-27 18:23:25 +0200

    Removed WSJ corpus dependent integration test.

--- src/mulm/model/test/regression.lisp
+++ src/mulm/model/test/regression.lisp
@@ -6,8 +6,10 @@
 (defun almost= (x y &key (epsilon 0.0001))
   (< (abs (- x y)) epsilon))
 
-(deftestsuite regression (all-regression)
-              ((profile (mime::perform-experiment *wsj-order2-expr-file*)))
-  (:tests ((loop for fold in (mime::profile-folds profile)
-                   for correct in '(91858 91831 92161 92103 92022 91880 91357 92769 92272 93393)
-                   do (ensure-same (mime::fold-token-correct fold) correct)))))
+; Test no longer valid
+
+; (deftestsuite regression (all-regression)
+;               ((profile (mime::perform-experiment *wsj-order2-expr-file*)))
+;   (:tests ((loop for fold in (mime::profile-folds profile)
+;                    for correct in '(91858 91831 92161 92103 92022 91880 91357 92769 92272 93393)
+;                    do (ensure-same (mime::fold-token-correct fold) correct)))))
